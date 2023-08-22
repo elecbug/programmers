fdb0a78845cd3b5e0661fbe331eecd9fb3835b47
deveb1479@gmail.com <deveb1479@gmail.com>
2023-08-22 16:20:24 +0900

    edit format 42577

--- 42577.cpp
+++ 42577.cpp
@@ -5,15 +5,11 @@
 
 using namespace std;
 
-bool solution(vector<string> phoneBook)
-{
+bool solution(vector<string> phoneBook) {
     sort(phoneBook.begin(), phoneBook.end());
-    for (int i = 0; i < phoneBook.size() - 1; i++)
-    {
-        if (phoneBook[i + 1].find(phoneBook[i]) != -1)
-        {
-            if (phoneBook[i] == phoneBook[i + 1].substr(0, phoneBook[i].size()))
-            {
+    for (int i = 0; i < phoneBook.size() - 1; i++) {
+        if (phoneBook[i + 1].find(phoneBook[i]) != -1) {
+            if (phoneBook[i] == phoneBook[i + 1].substr(0, phoneBook[i].size())) {
                 return false;
             }
         }
